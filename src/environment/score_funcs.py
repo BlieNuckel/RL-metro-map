3a0df904f4a2aab86413264635112f0a59c44872
Lasse Poulsen <work.lasse.face@gmail.com>
2023-03-21 08:25:13 +0100

    Reward Functions v12

--- src/environment/score_funcs.py
+++ src/environment/score_funcs.py
@@ -3,9 +3,9 @@ import math
 C_LINE_OVERLAP = 20
 C_STOP_OVERLAP = 20
 C_OUT_OF_BOUNDS = 20
-C_STOP_ADJACENCY = 6
-C_STOP_RELATIVE_POS = 4
-C_STOP_DISTRIBUTION = 4.5
+C_STOP_ADJACENCY = 7
+C_STOP_RELATIVE_POS = 6
+C_STOP_DISTRIBUTION = 6.5
 C_MINIMIZE_TURNS = 10
 C_PROMOTE_SPREADING = 1.5
 C_FINISHED = 20
@@ -32,25 +32,23 @@ def out_of_bounds() -> float:
 
 
 def stop_adjacency(stop_placed_adjacent: bool) -> float:
-    return C_STOP_ADJACENCY * (2 / 5 if stop_placed_adjacent else -1)
+    return C_STOP_ADJACENCY * (2 / 5 if stop_placed_adjacent else -1)  # 2/5 -> 1
 
 
 def stop_distribution(distributed_correctly: bool) -> float:
-    return C_STOP_DISTRIBUTION * (2 / 5 if distributed_correctly else -1)
+    return C_STOP_DISTRIBUTION * (2 / 5 if distributed_correctly else -1)  # 2/5 -> 1
 
 
 def stop_relative_position(angle_difference: float) -> float:
     if abs(angle_difference) < 22.5:
         return C_STOP_RELATIVE_POS * 1
     else:
-        return C_STOP_RELATIVE_POS * (
-            math.e ** ((-abs(angle_difference) + 22.5) / 4)
-        )  # (((math.e / 2) ** (-abs(angle_difference) + 24.75)) - 1)
+        return C_STOP_RELATIVE_POS * (((math.e / 2) ** (-abs(angle_difference) + 24.75)) - 1)
 
 
 def minimize_turns(num_of_recent_turns: int, max_recent_turns: int) -> float:
     if num_of_recent_turns <= 0:
-        return C_MINIMIZE_TURNS * 1
+        return C_MINIMIZE_TURNS * 1  # 1 -> 0
 
     return C_MINIMIZE_TURNS * _clamp_min(-1, -(2 ** (num_of_recent_turns - max_recent_turns)) + 1)
 
@@ -62,11 +60,11 @@ def minimize_turns(num_of_recent_turns: int, max_recent_turns: int) -> float:
 #     return C_PROMOTE_SPREADING * _clamp_min(0, 0.2 * math.log10(distance_from_start))
 
 
-def time_alive(time_step: int) -> float:
-    if time_step <= 0:
-        return C_TIME_ALIVE * 0
+# def time_alive(time_step: int) -> float:
+#     if time_step <= 0:
+#         return C_TIME_ALIVE * 0
 
-    return C_TIME_ALIVE * _clamp_max(1, 0.1 * time_step)
+#     return C_TIME_ALIVE * _clamp_max(1, 0.1 * time_step)
 
 
 def finished() -> float:
